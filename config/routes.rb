f2e0824ba88ebaa3f31d7458812dad28cd750034
kenta moriuchi <naturesciense@gmail.com>
2025-08-22 11:39:10 +0900

    change routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -2,8 +2,7 @@ Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
-  # root "articles#index"
+  root "articles#index"
   # localhost:3000/posts/
   resources :posts, only: [:index, :new, :create, :edit, :update, :destroy]
-  root "posts#index"
 end
